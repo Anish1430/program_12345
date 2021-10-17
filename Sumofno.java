70a8b21fdb51cbca332eb822c2da4cf4b480bfdc
Anish Kumar Sharma <80304547+Anish1430@users.noreply.github.com>
2021-10-17 19:10:16 +0530

    Update Sumofno.java

--- Sumofno.java
+++ Sumofno.java
@@ -5,15 +5,11 @@ public class Sumofno {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i, num = 10, sum = 0;  
-		//executes until the condition returns true  
-		for(i = 1; i <= num; i++)  
-		{  
-		//adding the value of i into sum variable  
+		 
+		for(i = 1; i <= num; i++) {    
 		sum = sum + i;  
 		}  
 		//prints the sum   
 		System.out.println("Sum of First 10 Natural Numbers is = " + sum);
-
 	}
-
 }
