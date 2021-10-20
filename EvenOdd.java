8f651cae611feea77a7844011ec2455af677c35c
Anish Kumar Sharma <80304547+Anish1430@users.noreply.github.com>
2021-10-20 15:41:33 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,18 @@
+package even;
+import java.util.*;
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(System.in);
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		if(x%2==0)
+		{
+			System.out.println("Even");
+		}
+		else {
+			System.out.println("Odd");
+		}
+	}
+}
